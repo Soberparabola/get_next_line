759f76381bd27495b2ff0993931d2b89a566a582
Jordi Garcia Saenz <jordgarc@cbr14s4.42barcelona.com>
2024-03-07 18:33:01 +0100

    nthng changed

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: jordgarc <jordgarc@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 17:44:02 by jordgarc          #+#    #+#             */
-/*   Updated: 2024/02/28 17:44:05 by jordgarc         ###   ########.fr       */
+/*   Updated: 2024/03/07 18:31:29 by jordgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,26 +70,26 @@ char	*ft_strchr(char *s, int c)
 char	*ft_substr(char *s, unsigned int start, size_t len)
 {
 	size_t	i;
-	char	*res;
+	char	*result;
 
 	i = 0;
 	if (!s)
 		return (0);
 	if (start > ft_strlen(s))
 	{
-		res = malloc(sizeof(char) * (1));
-		if (!res)
+		result = malloc(sizeof(char) * (1));
+		if (!result)
 			return (NULL);
-		res[0] = '\0';
-		return (res);
+		result[0] = '\0';
+		return (result);
 	}
 	if (ft_strlen(s) - start < len)
 		len = ft_strlen(s) - start;
-	res = malloc(sizeof(char) * (len + 1));
-	if (!res)
+	result = malloc(sizeof(char) * (len + 1));
+	if (!result)
 		return (NULL);
 	while (start < ft_strlen(s) && i < len && s[start])
-		res[i++] = s[start++];
-	res[i] = '\0';
-	return (res);
+		result[i++] = s[start++];
+	result[i] = '\0';
+	return (result);
 }
